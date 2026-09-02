f1dc8e9e4ae1bc0bc2498c016d3be3536608b311
mahfuz640 <mdmahfuz640@gmail.com>
2026-09-02 15:34:59 +0600

    previous

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: { proxy: { '/api': 'http://localhost:5000' } },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'three-vendor': ['three'],
-          'motion-vendor': ['gsap'],
-          'vision-vendor': ['@mediapipe/tasks-vision'],
-        },
-      },
-    },
-  },
 })
